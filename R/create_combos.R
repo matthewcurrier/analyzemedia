6fb8cd1c0f1904938b63bdf6eae400db426aa6b1
matthewcurrier <currier.matthew@gmail.com>
2017-10-25 15:45:42 -0700

    Initial commit

---
+++ R/create_combos.R
@@ -0,0 +1,25 @@
+#' Create list of all possible combinations of string vector
+#'
+#' This function has two arguments.
+#' @param strings Name of package to check
+#' @param comb Name of package to check
+#' @keywords sem, paid search
+#' @export
+#' @examples
+#' strings <- c("dog","cat", "mouse", "hampster", "horse", "bird")
+#' cc <- create_combos(strings, 3)
+#' print(cc)
+
+
+
+create_combos <- function(strings, comb) {
+  library(combinat)
+
+  f <- function(x) {
+    paste(x, collapse = ", ")
+  }
+
+  c <- combn(strings, comb, simplify = T)
+  df <- as.data.frame(c, stringsAsFactors=F)
+  l <- lapply(df, f)
+}
---
+++ R/media_summarize.R
@@ -0,0 +1,37 @@
+#' Summarize paid search data quickly.
+#'
+#' This function has five arguments.
+#' @param df Dataframe to analyze.
+#' @param spend Amount spent for observation.
+#' @param impr Number of impressions.
+#' @param clicks Number of clicks.
+#' @param ... Variables to group results by.
+#' @keywords paid search, sem, clicks, impressions, cpm, cpc
+#' @export
+#' @examples
+#' media_summarize(df, spend, impr, clicks, country, city)
+
+
+# This works!
+media_summarize <- function(df=df, spend=spend, impr=impr, clicks=clicks, ...) {
+  spend <- enquo(spend)
+  impr <- enquo(impr)
+  clicks <- enquo(clicks)
+  group_var <- quos(...)
+  #group_var <- enquo(group_var)
+
+  sp_q <- paste0("sum_", quo_name(spend))
+  im_q <- paste0("sum_", quo_name(impr))
+  cl_q <- paste0("sum_", quo_name(clicks))
+
+
+  df %>%
+    group_by(!!!group_var) %>%
+    summarise(sp_q := sum(!!spend),
+              im_q := sum(!!impr),
+              cl_q := sum(!!clicks),
+              cpm = sum((!!spend) / (sum(!!impr) / 1000)),
+              cpc = sum(!!spend) / sum(!!clicks)
+    ) %>%
+    arrange(cpm)
+}
