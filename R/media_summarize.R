9c159b444e7e1609896372355488fc3a62d32991
matthewcurrier <currier.matthew@gmail.com>
2017-10-27 15:22:22 -0700

    misc stuff to commit

--- R/media_summarize.R
+++ R/media_summarize.R
@@ -20,16 +20,12 @@ media_summarize <- function(df=df, spend=spend, impr=impr, clicks=clicks, ...) {
   group_var <- quos(...)
   #group_var <- enquo(group_var)
 
-  sp_q <- paste0("sum_", quo_name(spend))
-  im_q <- paste0("sum_", quo_name(impr))
-  cl_q <- paste0("sum_", quo_name(clicks))
-
 
   df %>%
     group_by(!!!group_var) %>%
-    summarise(sp_q := sum(!!spend),
-              im_q := sum(!!impr),
-              cl_q := sum(!!clicks),
+    summarise(spend := sum(!!spend),
+              impr := sum(!!impr),
+              clicks := sum(!!clicks),
               cpm = sum((!!spend) / (sum(!!impr) / 1000)),
               cpc = sum(!!spend) / sum(!!clicks)
     ) %>%
---
+++ R/ts_summarize.R
@@ -0,0 +1,29 @@
+#' Summarize time series based data quickly.
+#'
+#' This function has five arguments.
+#' @param df Dataframe to analyze.
+#' @param date Name of date column for dataset.
+#' @param value Number of something.
+#' @param ... Variables to group results by.
+#' @keywords percent change, previous quarter, previous year
+#' @export
+#' @examples
+#' ts_summarize(df, spend, impr, clicks, country, city)
+
+ts_summarize <- function(df, date, value, ...) {
+  value <- enquo(value)
+  group_var <- quos(...)
+  date <- enquo(date)
+
+  df <- df %>% arrange(!!date, !!!group_var)
+
+  df <- df %>%
+    group_by(!!!group_var) %>%
+    mutate(lag_value=lag(value),
+           per_change=format((value - lag_value) / value, digits=3)
+           ) %>%
+    ungroup()
+}
+
+
+
